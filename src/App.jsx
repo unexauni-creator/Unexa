0655034df77da06c6e05b6b55cdc46d3fc089507
Kateryna <katedmitrenko15@gmail.com>
2026-09-23 09:55:54 +0200

    Fix missing student-life.css import in App.jsx, causing unstyled Student Life page

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,7 @@ import "./styles/community.css";
 import "./styles/landing.css";
 import "./styles/profile.css";
 import "./styles/university-detail.css";
+import "./styles/student-life.css";
 import "./styles/teachers-section.css";
 import Onboarding from "./pages/Onboarding";
 import ForgotPassword from "./pages/ForgotPassword";
